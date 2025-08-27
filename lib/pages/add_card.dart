ca81dbb643d7479e813a5b7f076885b2c927952f
Ricardo da Silva Ogliari <rogliariping@gmail.com>
2025-08-27 20:01:06 -0300

    add pages and routes

---
+++ lib/pages/add_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddCard extends StatelessWidget {
+  const AddCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
